9bc617bfb91bb41a9bdbb870ccab4f06f7fa06b5
santiago-pl01 <palomasantiago134@gmail.com>
2026-02-10 19:21:44 -0300

    add_login_e_cadastro
    Aqui estão as telas de login e de cadastro. Ambas ainda não funcionais, nesta branch está apenas a parte estética

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,15 +1,23 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          headerShown: true,
-          title: "Login",
+    <>
+
+      <StatusBar
+        backgroundColor="#21155d"  // cor da barra (Android)
+        barStyle="light-content"  // ícones brancos
+      />
+
+      <Stack
+        screenOptions={{
+          headerShown: false, // oculta o header para as telas de autenticação
+          
         }}
       />
-    </Stack>
+
+    </>
+
   );
 }
--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,9 +1,119 @@
-import { View } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, Button } from "react-native";
+import { router } from "expo-router";
+
 
 export default function RegisterPage() {
   return (
     <>
-      <View></View>
+      <View style={styles.container}>
+        <View>
+          {/* Título e linha decorativa */}
+          <Text style={styles.title}>
+            Registrar-se
+          </Text>
+
+          {/* Botão para ir para a página de registro */}
+          <TouchableOpacity onPress={() => router.push("/(auth)")}>
+            <Text style={styles.subtitle}>Já tem conta? Faça login</Text>
+          </TouchableOpacity>
+
+          <Text style={styles.line} >________________________________________________</Text>
+
+          {/*campo de email e senha*/}
+
+          <View>
+
+            <Text style={styles.text}>Nome Completo:</Text>
+            <TextInput
+              placeholder="Digite seu nome completo"
+              style={styles.input}
+            />
+
+            <Text style={styles.text}>email:</Text>
+            <TextInput
+              placeholder="Digite seu email"
+              style={styles.input}
+            />
+
+            <Text style={styles.text}>Insira sua senha :</Text>
+            <TextInput
+              placeholder="Digite sua senha"
+              style={styles.input}
+            />
+
+            <Text style={styles.text}>Insira sua senha :</Text>
+            <TextInput
+              placeholder="Comfirme sua senha"
+              style={styles.input}
+            />
+
+          </View>
+          {/* Botão para ir para o Dashboard */}
+          <View >
+            <Button
+              title="Dashboard"
+              onPress={() => router.push("/(dashboard)")}
+            />
+          </View>
+        </View>
+      </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#21155d",
+    alignItems: "center",         // centraliza na horizontal
+    paddingTop: 40,
+    height: "100%", // garante que o container ocupe toda a altura
+    width: "100%", // garante que o container ocupe toda a largura
+
+  },
+
+
+  title: {
+    color: "#fff",
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 120,    // espaço entre o topo e o título
+    marginBottom: 10,   // espaço entre o título e a linha
+
+  },
+
+  text: {
+    color: "#f2e7fe",
+  },
+
+  subtitle: {
+    color: "#f2e7fe",
+    fontSize: 10,
+    textAlign: "center",
+    marginBottom: 20,
+  },
+
+  line: {
+    width: "80%",       // tamanho da linha
+    height: 1,          // espessura
+    backgroundColor: "#bebdda", // cor roxinha (ajusta se quiser)
+    marginTop: 10,
+    marginBottom: 20,   // espaço depois da linha
+    alignItems: "center",
+
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 8,
+    fontSize: 16,
+    marginBottom: 20,   // espaço 
+    alignItems: "center",
+
+  },
+
+});
